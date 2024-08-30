12d6976be4c38b78d0e2fbaa192f1d2fa3a0616d
lindavataksi <lindavataksi3@gmail.com>
2024-08-30 00:18:23 -0400

    Set up input box

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // auto import by pressing ctrl space
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 // App -cosnt arrow function instead of function
 // App type is defined as a react functional component
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string> ("")
+  const[todos,setTodos] = useState<Todo[]>([])
+  // console.log(todo);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
+      setTodo("");
+    }
+  };
   return (
     <div className="App">
       <span className='heading'> Taskify</span>
-      <InputField />
+      <InputField todo = {todo} setTodo = {setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,11 +2,23 @@
 // BEM Classnaming convention
 import React from 'react'
 import './styles.css'
+interface Props {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+    handleAdd: (e: React.FormEvent) => void;
+}
+// const InputField = ({todo, setTodo}: Props) => {
 
-const InputField = () => {
+ const InputField:React.FC <Props>= ({todo, setTodo, handleAdd}) => {
   return (
     <form action="" className="input">
-        <input type='input' placeholder="Enter a task" className='input_box' />
+        <input 
+        value = {todo}
+        onChange={
+            (e) => setTodo(e.target.value)
+        }
+        onSubmit = {handleAdd}
+        type='input' placeholder="Enter a task" className='input_box' />
         <button className="input_submit" type='submit'> Go</button>
     </form>
   )
